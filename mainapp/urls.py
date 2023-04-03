4d05515535bbc61beb7c98771312d65552d5b8ac
107131n <ddolgi1206@nate.com>
2023-04-03 18:55:25 +0900

    'web'

---
+++ mainapp/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+
+###  from 뒤에 작성 규칙
+#   - 폴더 경로 또는 폴더 경로 + 파일명
+###
+
+from . import views # .: 현재 위치
+
+urlpatterns = [
+    ### http://127.0.0.1:8000/
+    # path('', views.index),
+    # path('index/', views.index),   
+]
---
+++ secondapp/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+###  from 뒤에 작성 규칙
+#   - 폴더 경로 또는 폴더 경로 + 파일명
+
+from . import views
+
+urlpatterns = [
+    ### http://127.0.0.1:8000/second/
+    path('', views.index),
+    path('index/', views.index),
+]
